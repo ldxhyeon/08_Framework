a8e7dabaec07b7df391423a2269cfdac80485ffa
ldxhyeon <dlehdgus3180@naver.com>
2024-09-05 17:46:11 +0900

    2024_09_05 Spring,Thymeleaf 학습

---
+++ dempProject3/src/main/java/edu/kh/demo/DempProject3Application.java
@@ -0,0 +1,13 @@
+package edu.kh.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DempProject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DempProject3Application.class, args);
+	}
+
+}
