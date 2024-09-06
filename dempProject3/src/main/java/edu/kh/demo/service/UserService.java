db9333c185f52a0e3bb4419f2cb5f39aaf002ce9
ldxhyeon <dlehdgus3180@naver.com>
2024-09-06 17:49:14 +0900

    2024_09_06 Spring todoList

--- dempProject3/src/main/java/edu/kh/demo/service/UserService.java
+++ dempProject3/src/main/java/edu/kh/demo/service/UserService.java
@@ -1,11 +1,53 @@
 package edu.kh.demo.service;
 
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+
+import edu.kh.demo.dto.UserDto;
+
 public interface UserService {
-	
+
 	/** 사용자 이름 조회 
 	 * @param userNo
 	 * @return userName
 	 */
 	String selectUserName(int userNo);
+
+	
+	/** 사용자 전체 조회
+	 * @return userList
+	 */
+	List<UserDto> selectAll();
+	
+	
+	
+	/** userNo가 일치하는 사용자 조회
+	 * @param userNo
+	 * @return
+	 */
+	UserDto selectUser(int userNo);
+
+
+	/** 사용자 정보 수정
+	 * @param user
+	 * @return
+	 */
+	int updateUser(UserDto user);
+
+
+	/** 사용자 정보 삭제
+	 * @param user
+	 * @return
+	 */
+	int deleteUser(int userNo);
+
+
+	
+	int insertUser(UserDto user);
+
+
+	
+	
 
 }
--- dempProject3/src/main/java/edu/kh/demo/service/UserServiceImpl.java
+++ dempProject3/src/main/java/edu/kh/demo/service/UserServiceImpl.java
@@ -1,8 +1,17 @@
 package edu.kh.demo.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import edu.kh.demo.dto.UserDto;
 import edu.kh.demo.mapper.TestMapper;
 
 // @Service
@@ -29,11 +38,59 @@ public class UserServiceImpl implements UserService {
 	 */
 	@Override
 	public String selectUserName(int userNo) {
-		
-		
-		
 		return mapper.selectUserName(userNo);
 	}
+	
+	
+	@Override
+	public List<UserDto> selectAll() {
+		// mapper : 의존성 주입(DI) 받은 TestMapper를
+		// 				  상속 받아 구현된 클래스로 만들어진 Bean(객체)
+		return mapper.selectAll();
+	}
+	
+	
+	// userNo가 일치하는 사용자 조회
+	@Override
+	public UserDto selectUser(int userNo) {
+		return mapper.selectUser(userNo);
+	}
+
+	
+	// 사용자 정보 수정(DML)
+	// -> DML 수행하면 트랜잭션 제어 처리
+	// @Transactional : 해당 메서드 수행 중 RuntimeException 발생 시 rollback 수행
+	// 예외가 발생하지 않으면 메서드 종료 후 commit 수행
+	@Transactional
+	@Override
+	public int updateUser(UserDto user) {
+		return mapper.updateUser(user);
+	}
+	
+	
+	
+	// 사용자 삭제
+	@Transactional // 예외 발생 시 rollback, 아니면 메서드 종료 시 commit
+	@Override
+	public int deleteUser(int userNo) {
+		return mapper.deleteUser(userNo);
+	}
+	
+	
+	
+	// 사용자 추가
+	@Transactional
+	@Override
+	public int insertUser(UserDto user) {
+		return mapper.insertUser(user);
+	}
+	
+	
+	
+
+	
+	
+	
 	
 
 }
