f6984d447a5c1884733a8ca369f5ccfeb2a1ffea
ldxhyeon <dlehdgus3180@naver.com>
2024-10-20 23:24:45 +0900

    2024_10_20 myPage-boad UI

--- RepatPractice/src/main/java/repet/ldh/project/mypage/controller/MyPageController.java
+++ RepatPractice/src/main/java/repet/ldh/project/mypage/controller/MyPageController.java
@@ -2,13 +2,33 @@ package repet.ldh.project.mypage.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("myPage")
 public class MyPageController {
 	
-	@GetMapping("mypage")
-	public String mypage() {
-		return "myPage/myPage-profile";
+	@GetMapping("info")
+	public String myPage() {
+		return "myPage/myPage-info";
+	}
+	
+	
+	@GetMapping("changeInfo")
+	public String changeInfo() {
+		return "myPage/myPage-changeInfo";
+	}
+	
+	
+	@GetMapping("board")
+	public String myBoard() {
+		return "myPage/myPage-board";
+	}
+	
+	
+	@GetMapping("delete")
+	public String delete() {
+		return "myPage/myPage-delete";
 	}
 
 }
