a7b10ab2f11872dbf7769d81a591cc368ca0b382
ldxhyeon <dlehdgus3180@naver.com>
2024-10-24 12:21:13 +0900

    Mypage

--- RepatPractice/src/main/java/repet/ldh/project/main/service/MainService.java
+++ RepatPractice/src/main/java/repet/ldh/project/main/service/MainService.java
@@ -2,7 +2,6 @@ package repet.ldh.project.main.service;
 
 import java.util.List;
 
-import repet.ldh.project.board.dto.Board;
 
 public interface MainService {
 
--- RepatPractice/src/main/java/repet/ldh/project/main/service/MainServiceImpl.java
+++ RepatPractice/src/main/java/repet/ldh/project/main/service/MainServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
-import repet.ldh.project.board.dto.Board;
 import repet.ldh.project.main.mapper.MainMapper;
 
 @Service
